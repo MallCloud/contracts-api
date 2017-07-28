3b4e27904566a3a9825c79bebce244e46e110fbe
daemonslayer <kpgupta98@gmail.com>
2017-07-28 11:55:51 +0530

    Data schema

---
+++ src/schema/DataType.js
@@ -0,0 +1,8 @@
+/* @flow */
+
+import { GraphQLObjectType, GraphQLList, GraphQLNonNull, GraphQLInt, GraphQLString } from 'graphql';
+import { globalIdField } from 'graphql-relay';
+
+export default new GraphQLObjectType ({
+  
+});
