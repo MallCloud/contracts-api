0fdf34c87e444f8029f001d904e5980163688685
daemonslayer <kpgupta98@gmail.com>
2017-07-28 11:56:30 +0530

    Transaction Schema

---
+++ src/schema/Transaction.js
@@ -0,0 +1,8 @@
+/* @flow */
+
+import validator from 'validator';
+import { GraphQLNonNull, GraphQLID, GraphQLString } from 'graphql';
+import { fromGlobalId, mutationWithClientMutationId } from 'graphql-relay';
+
+import db from '../db';
+import TransactionType from './TransactionType';
