80613a8dda70641c48a69f00cf1e3f1f4e543230
daemonslayer <kpgupta98@gmail.com>
2017-08-16 17:48:53 +0530

    ENS Market Ethereum Name Service : A distributed, open, and extensible naming system based on the Ethereum blockchain. https://docs.ens.domains/en/latest/introduction.html

---
+++ gatecon/contracts/ENSMarket/ENSMarket.sol
@@ -0,0 +1,73 @@
+import './ENS/AbstractENS.sol';
+import '../PublicMarket.sol';
+import '../DINRegistry.sol';
+import '../OrderTracker.sol';
+
+pragma solidity ^0.4.11;
+
+contract ENSMarket is PublicMarket {
+
+	string public title = "ENS";
+
+	// ENS Registry
+	AbstractENS public ens;
+
+	struct ENSDomain {
+		string name;
+		bytes32 node;
+	}
+
+	// DIN => ENS node
+	mapping(uint256 => ENSDomain) public domains;
+
+	// Constructor
+	function ENSMarket(
+		DINRegistry _dinRegistry, 
+		OrderTracker _orderTracker, 
+		AbstractENS _ens)
+		PublicMarket(
+			_dinRegistry, 
+			_orderTracker
+		)
+	{
+		ens = _ens;
+	}
+
+	function orderInfo(uint256 DIN) constant returns (bytes32) {
+		return ENSNode(DIN);
+	}
+
+	function isFulfilled(uint256 orderID) constant returns (bool) {
+		// Get the ENS node from the order
+		bytes32 node = orders[orderID].info;
+
+		// Check that buyer is the owner
+		return (ens.owner(node) == orders[orderID].buyer);
+	}
+
+	function availableForSale(uint256 DIN, uint256 quantity) constant returns (bool) {
+		// The owner of the node must be able to transfer it during a purchase.
+		if (ens.owner(ENSNode(DIN)) != buyHandler(DIN)) {
+			return false;
+		}
+
+		return PublicMarket.availableForSale(DIN, quantity);
+	}
+
+	function name(uint256 DIN) constant returns (string) {
+		return domains[DIN].name;
+	}
+
+	function setName(uint256 DIN, string name) only_owner(DIN) {
+		domains[DIN].name = name;
+	}
+
+	function ENSNode(uint256 DIN) constant returns (bytes32) {
+		return domains[DIN].node;
+	}
+
+	function setENSNode(uint256 DIN, bytes32 node) only_owner(DIN) {
+		domains[DIN].node = node;
+	}
+
+}
---
+++ gatecon/contracts/ENSMarket/ENSPublicProduct.sol
@@ -0,0 +1,92 @@
+import '../Product.sol';
+import '../DINRegistry.sol';
+import '../Market.sol';
+import './ENSMarket.sol';
+import './ENS/ENS.sol';
+
+pragma solidity ^0.4.11;
+
+/**
+*  This is an example of how to sell an ENS domain.
+*/
+contract ENSPublicProduct is Product {
+
+	ENSMarket public ensMarket;
+	ENS public ens;
+
+	struct ENSNode {
+		string name;
+		bytes32 node;
+		uint256 price;
+		bool isValid;
+	}
+
+	// DIN => ENS node
+	mapping(uint256 => ENSNode) public nodes;
+
+	// Constructor
+	function ENSPublicProduct(
+		DINRegistry _registry,
+		ENSMarket _market,
+		ENS _ens
+	)
+		Product(
+			_market,
+			_registry
+		)
+	{
+		ensMarket = _market;
+		ens = _ens;
+	}
+
+	function addENSDomain(uint256 DIN, string name, bytes32 node, uint256 price) {
+		// Store the details of the ENS node.
+		nodes[DIN].name = name;
+		nodes[DIN].node = node;
+		nodes[DIN].price = price;
+		nodes[DIN].isValid = true;
+	}
+
+	function name(uint256 DIN) constant returns (string) {
+		return nodes[DIN].name;
+	}
+
+	function setName(uint256 DIN, string name) only_owner(DIN) {
+		nodes[DIN].name = name;
+	}
+
+	function node(uint256 DIN) constant returns (bytes32) {
+		return nodes[DIN].node;
+	}
+
+	function setNode(uint256 DIN, bytes32 node) only_owner(DIN) {
+		nodes[DIN].node = node;
+	}
+
+	// Quantity is irrelevant here. We're only ever selling one ENS domain at a time
+	function totalPrice(uint256 DIN, uint256 quantity, address buyer) constant returns (uint256) {
+		// // Let the buyer buy back his domain (remove it from the market) for free.
+		// if (buyer == registry.owner(DIN)) {
+		// 	return 0;
+		// }
+		return nodes[DIN].price;
+	}
+
+	function setPrice(uint256 DIN, uint256 price) only_owner(DIN) {
+		nodes[DIN].price = price;
+	}
+
+	function isAvailableForSale(uint256 DIN, uint256 quantity) constant returns (bool) {
+		return nodes[DIN].isValid;
+	}
+
+	function handleOrder(uint256 orderID, uint256 DIN, uint256 quantity, address buyer) only_market {
+		// The buyer is only getting a single domain.
+		require(quantity == 1);
+
+		// Give ownership of the node to the buyer.
+		// Make sure to set the owner of the node to this contract first.
+		ens.setOwner(nodes[DIN].node, buyer);
+	}
+
+}
