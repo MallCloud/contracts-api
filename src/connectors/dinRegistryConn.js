8bf7aaac5f9f118f9f080030a5e7cbc2bd849d84
daemonslayer <kpgupta98@gmail.com>
2017-08-26 07:33:49 +0530

    comments on din connector

--- src/connectors/dinRegistryConn.js
+++ src/connectors/dinRegistryConn.js
@@ -12,18 +12,37 @@ var requireNoCache = require("require-nocache")(module);
 var provider = new Web3.providers.HttpProvider("http://localhost:8545");
 var temp = Temp.track();
 
+/**
+ * Provides functions from the DIN Registry Contract
+ */
 const DINConnectorInstance = {
+    // instance of contract; used for calling functions of a contract
     contractInst: null,
+    // address of contract deployed on a network
     contractAddr: null,
+    // address to which any transactions are pointed towards
     toAddress: null,
+    // amount of transaction
     toAmount: 0,
+    // object of web3
     web3: null,
+    // provider of the web3 network
     web3Provider: null,
+    // balance in account of the user
     balance: 0,
+    // all available contracts in a list
     contracts: {},
+    // account of the user
     account: null,
+    // logs in details received by callback
     logger: null,
 
+    /**
+     * added to an event in order to send back the result of query
+     * sent to event that triggers the respective event.
+     * @param  {[string]} error  [error log for fn that triggered event]
+     * @param  {[string]} result [result of the fn]
+     */
     callback: function(error, result) {
         if (!error) {
             logger = result;
@@ -33,6 +52,9 @@ const DINConnectorInstance = {
         }
     },
 
+    /**
+     * used to select the server on which contracts are live
+     */
     getProvider: function() {
         if(this.web3 !== null) {
             this.web3Provider = this.web3.currentProvider;
@@ -43,12 +65,18 @@ const DINConnectorInstance = {
         }
     },
 
+    /**
+     * take the abi file and connect contract corresponding to it
+     */
     getContract: function() {
         var dinRegistryArtifact = require('../contracts/DINRegistry.json');
         this.contracts.DINRegistry = contract(dinRegistryArtifact);
         this.contracts.DINRegistry.setProvider(this.web3Provider);
     },
 
+    /**
+     * checks which network are we on
+     */
     networkCheck: function() {
         this.web3.version.getNetwork((err, netId) => {
             if (err) {
@@ -72,6 +100,10 @@ const DINConnectorInstance = {
         });
     },
 
+    /**
+     * get all available accounts on a network
+     * @return {[list]} [list of accounts available]
+     */
     getAccounts: function() {
         this.web3.eth.getAccounts((error, accounts) => {
             if (error) {
@@ -82,6 +114,12 @@ const DINConnectorInstance = {
         });
     },
 
+    /**
+     * connects to a deployed contract &
+     * creates a DIN using the remote contract
+     * @param  {[uint]} account [account of the person in transaction]
+     * @return {[uint]} din     [newly minted DIN]
+     */
     createNewDIN: function(account) {
         this.getProvider();
         // this.networkCheck();
@@ -97,7 +135,6 @@ const DINConnectorInstance = {
                 var event = contractInstance.NewRegistration({owner: this.account});
                 event.watch(this.callback);
 
-                console.log('give me my logger, how can it be ' + logger);
                 return contractInstance.registerDIN({from: this.account})
                     .then(function() {
                         event.stopWatching();
